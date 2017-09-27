a71d7fdf0f7aac8ed5dc7d092017d95a6fac3b70
Unknown <ironcev@hotmail.com>
2017-09-27 23:16:48 +0200

    feat: Turns IntelliSense on and off

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,52 @@
-'use strict'; 
+'use strict';
 
-import * as vscode from 'vscode'; 
+import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) { 
-   console.log('Congratulations, your extension "without-guns" is now active!'); 
+export function activate(context: vscode.ExtensionContext) {
+    const intelliSenseSettings = new IntelliSenseSettings(vscode.workspace.getConfiguration())
 
-   let disposable = vscode.commands.registerCommand('withoutGuns.gunsOff', () => { 
-       vscode.window.showInformationMessage('Your guns are turned off ;-)'); 
-   }); 
+    let gunsOff = vscode.commands.registerCommand('withoutGuns.gunsOff', () => {
+       vscode.window.showInformationMessage('Your guns are turned off ;-)');
+       intelliSenseSettings.removeIntelliSense();
+    });
 
-   context.subscriptions.push(disposable); 
+    let gunsOn = vscode.commands.registerCommand('withoutGuns.gunsOn', () => {
+        vscode.window.showInformationMessage('Your guns are turned on.');
+        intelliSenseSettings.applyInitialSettings();
+    });
+
+   context.subscriptions.push(gunsOff);
+   context.subscriptions.push(gunsOn);
+}
+
+class IntelliSenseSettings {
+    private readonly configuration : vscode.WorkspaceConfiguration;
+
+    private readonly initialQuickSuggestions : boolean;
+    private readonly initialWordBasedSuggestions : boolean;
+    private readonly initialParameterHints : boolean;
+    private readonly initialsuggestOnTriggerCharacters : boolean;
+
+    constructor(configuration : vscode.WorkspaceConfiguration) {
+        this.configuration = configuration;
+
+        this.initialQuickSuggestions = configuration.get("editor.quickSuggestions");
+        this.initialWordBasedSuggestions = configuration.get("editor.wordBasedSuggestions");
+        this.initialParameterHints = configuration.get("editor.parameterHints");
+        this.initialsuggestOnTriggerCharacters = configuration.get("editor.suggestOnTriggerCharacters");
+    }
+
+    applyInitialSettings() {
+        this.configuration.update("editor.quickSuggestions", this.initialQuickSuggestions);
+        this.configuration.update("editor.wordBasedSuggestions", this.initialWordBasedSuggestions);
+        this.configuration.update("editor.parameterHints", this.initialParameterHints);
+        this.configuration.update("editor.suggestOnTriggerCharacters", this.initialsuggestOnTriggerCharacters);
+    }
+
+    removeIntelliSense() {
+        this.configuration.update("editor.quickSuggestions", false);
+        this.configuration.update("editor.wordBasedSuggestions", false);
+        this.configuration.update("editor.parameterHints", false);
+        this.configuration.update("editor.suggestOnTriggerCharacters", false);
+    }
 }
