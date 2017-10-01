8c1a7d5ccff94dddba250aa3205e672f17b84fe6
Igor Rončević <ironcev@hotmail.com>
2017-10-01 21:41:00 +0200

    docs: Properly comments the base gun controller classes

--- src/ConfigurationDependentGunController.ts
+++ src/ConfigurationDependentGunController.ts
@@ -2,6 +2,15 @@ import * as vscode from 'vscode';
 import GunController from './GunController';
 import ConfigurationProvider from './ConfigurationProvider';
 
+/**
+ *  A gun controller that implements its behavior by modifying the Workspace Configuration.
+ * 
+ *  If a gun controller needs to manipulate VS Code settings (Workspace Configuration)
+ *  in order to achieve the control, then it should do the following:
+ *  - Store the current VS Code settings each time when taking the gun.
+ *  - The settings are stored in a private field called "initialSettings".
+ *  - Restore the settings when giving the gun back.
+ */
 export default abstract class ConfigurationDependentGunController extends GunController {
     constructor(private readonly configurationProvider : ConfigurationProvider) {
         super();
--- src/GunController.ts
+++ src/GunController.ts
@@ -1,11 +1,5 @@
 /**
  *  Controlls a single gun.
- * 
- *  If a gun controller needs to manipulate VS Code settings in order to achieve the control
- *  it should do the following:
- *  - Store the current VS Code settings each time when taking the gun.
- *  - The settings are stored in a private field called "initialSettings".
- *  - Restore the settings when giving the gun back.
  */
 export default abstract class GunController {
     /** True if the gun is currently taken. We assume that the gun is initially never taken. */
