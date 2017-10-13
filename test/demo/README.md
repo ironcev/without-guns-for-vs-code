088435d2974b9ad1c935f56a3c6b939dc8a28abd
Igor Rončević <ironcev@hotmail.com>
2017-10-13 23:35:13 +0200

    test: Adds demo.ts

---
+++ test/demo/README.md
@@ -0,0 +1,3 @@
+# Demo
+
+This folder contains a single source file that is used to create the animation that demonstrates the usage of the extension.
---
+++ test/demo/demo.ts
@@ -0,0 +1,48 @@
+class IsStraightforward {
+    Straightforward() {}
+}
+
+class You {
+    You(abilities: string[]) {}
+}
+
+class IsGives {
+    Is() {
+        return new IsStraightforward();
+    }
+    Gives() {
+        return new You();
+    }    
+}
+
+class Guns {
+    Guns() {
+        return new IsGives();
+    }
+}
+
+class Coding {
+    With() {
+        return new Guns();
+    }
+    Without() {
+        return new Guns();
+    }
+}
+
+function demo() {
+    var coding = new Coding();
+
+    // Coding with guns. The usual way.
+    coding.With().Guns().Is().Straightforward();
+
+    // Coding without guns. 
+    coding
+        .Without()
+        .Guns()
+        .Gives()
+        .You([
+            "Confidence",
+            "Deep knowledge"
+        ]);
+}
